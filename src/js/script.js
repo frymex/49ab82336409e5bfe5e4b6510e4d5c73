43a3c391eb83e3b55e444a6029ee21e8e27e8186
frymex <t_frymex@protonmail.com>
2023-10-23 12:43:52 +0300

    Удалены все файлы

---
+++ src/js/script.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    // if (document.location.href.includes("/index.html")) {
+    //     history.pushState({}, "", "/");
+    // }
+
+    document.title = "Bio <3"
+    const profileDiv = document.createElement("div");
+    profileDiv.className = "profile";
+
+    const img = document.createElement("img");
+    img.src = "src/img/img.jpg";
+    img.alt = "profile";
+
+    const h2 = document.createElement("h2");
+    h2.textContent = "cazqev";
+
+    const p = document.createElement("p");
+    p.className = "description";
+    p.innerHTML = "Python developer. From Ukraine.<br><br><code id='description__second' onmouseover='onMouseOver()' onmouseout='onMouseOut()' onclick='onClick()'>Logica a puncto A ad punctum B te auferre potest, et imaginatio alicub</code>";
+
+    const telegramButton = document.createElement("a");
+    telegramButton.href = "tg://resolve?domain=cazqev";
+    telegramButton.className = "telegram-button";
+    telegramButton.textContent = "Write me";
+
+    profileDiv.appendChild(img);
+    profileDiv.appendChild(h2);
+    profileDiv.appendChild(p);
+    profileDiv.appendChild(telegramButton);
+    document.body.appendChild(profileDiv);
+});
+
+const texts = {
+    ru_text: "Логика может привести вас из точки А в точку Б, а воображение — куда угодно.",
+    lat_text: "Logica a puncto A ad punctum B te auferre potest, et imaginatio alicub"
+}
+
+function onClick() {
+    const codeElement = document.getElementById('description__second');
+
+    if (codeElement.innerText === texts.lat_text) {
+        codeElement.innerText = texts.ru_text;
+    } else {
+        codeElement.innerText = texts.lat_text;
+    }
+}
+
+function onMouseOver() {
+    const codeElement = document.getElementById('description__second');
+    codeElement.innerText = texts.ru_text;
+}
+
+function onMouseOut() {
+    const codeElement = document.getElementById('description__second');
+    codeElement.innerText = texts.lat_text;
+}
+
